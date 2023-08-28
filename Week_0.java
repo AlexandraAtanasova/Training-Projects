dbbdeb3fe3c6f25218f3f941f7325635aac20b35
AlexandraAtanasova <alexandra0ata@gmail.com>
2023-08-28 13:37:14 -0400

    Broilerplate + first print lines

--- Week_0.java
+++ Week_0.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class Week_0 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Madlibs Generator! Before we begin please choose a template.");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("> ");
+        String template = scanner.nextLine();
+        
+        System.out.println("You have chosen template" + template + "Follow the prompts below to complete story generation");
+
+        System.out.println();
+        scanner.close();
+
+        
+    }
+}
