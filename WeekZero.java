538751d81ba49145fc3ab034b9b11853e4f1fcbc
AlexandraAtanasova <alexandra0ata@gmail.com>
2023-08-31 13:13:38 -0400

    Renamed project file, unable to load class unsolved

--- WeekZero.java
+++ WeekZero.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Week_0 {
+public class WeekZero{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -10,7 +10,12 @@ public class Week_0 {
         System.out.println("> ");
         String template = scanner.nextLine();
         
-        System.out.println("You have chosen template" + template + "Follow the prompts below to complete story generation");
+        System.out.println("You have chosen template" + template + ". Follow the prompts below to complete story generation");
+
+        System.out.println("Let's start with our nouns, pick two proper nouns, one place, and one object.");
+        System.out.println("> ");
+        String proper_noun1 = scanner.nextLine();
+        System.out.println(proper_noun1);
 
         System.out.println();
         scanner.close();
