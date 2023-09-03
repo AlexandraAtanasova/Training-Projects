040cda67122a4d12fe9aa62325a8da1fbb500ae9
AlexandraAtanasova <140348919+AlexandraAtanasova@users.noreply.github.com>
2023-09-03 19:12:05 -0400

    Update WeekZero.java

--- WeekZero.java
+++ WeekZero.java
@@ -4,7 +4,7 @@ public class WeekZero{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to MadLibs generator, follow the upcoming prompts to complete generation.")
+        System.out.println("Welcome to MadLibs generator, follow the upcoming prompts to complete generation.");
         
         System.out.println("Let's start with our nouns, pick two proper nouns, one place, and one object.");
         System.out.println("- ");
